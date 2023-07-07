9e3105dee9e5d8ae1e8b6f1c835a79890f77f7ed
Yeon <yeonhwan619@gmail.com>
2023-07-07 15:06:13 +0900

    ⚙️ Init + Add scroll behavior and useDebounce

---
+++ app/sections/AboutMe.tsx
@@ -0,0 +1,3 @@
+export default function AboutMe() {
+  return <div className="bg-emerald-400 w-screen h-screen">AboutMe</div>;
+}
---
+++ app/sections/Index.tsx
@@ -0,0 +1,3 @@
+export default function Index() {
+  return <div className="bg-red-400 w-screen h-screen">Index</div>;
+}
